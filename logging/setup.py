371e391aec7926a483a4d40a98349ee59a7b2825
Edie Mendoza <ntarticpnguin@proton.me>
2025-02-04 11:16:49 -0600

    created python logger

---
+++ logging/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pylogger',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Simple Python Logging Utility',
+    author='Eduardo',
+    install_requires=[],
+    python_requires='>=3.10'
+)
